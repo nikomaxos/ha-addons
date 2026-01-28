48067144101667147d9880640c0f2ee2c697d35b
nikomaxos <pardasnikos@gmail.com>
2026-01-28 15:56:03 +0200

    remove run.sh

--- gemini_agent/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-bashio::log.info "Starting Jarvis AI Professional (v20.0)..."
-bashio::log.info "Initializing System..."
-
-# Εκτέλεση του python script με Unbuffered output (-u) για να βλέπεις τα logs αμέσως
-exec python3 -u /agent.py
