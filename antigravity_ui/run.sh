a26271c3561d64f304f37bfd5e8d38fcde2e00f8
Nick Pardas <pardasnikos@gmail.com>
2026-09-15 12:49:50 +0300

    Add user-defined password option for AntiWeb

--- antigravity_ui/run.sh
+++ antigravity_ui/run.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bashio
 API_KEY=$(bashio::config 'api_key')
+WEB_PASS=$(bashio::config 'web_password')
+
 bashio::log.info "Starting AntiWeb UI..."
 export GEMINI_API_KEY="$API_KEY"
 export PORT=8080
 export HOST=0.0.0.0
 export BIND=0.0.0.0
+
+if [ -n "$WEB_PASS" ] && [ "$WEB_PASS" != "null" ]; then
+    bashio::log.info "Web password is set by user."
+    export ANTIWEB_PASSWORD="$WEB_PASS"
+    export ANTIWEB_HTTP_PASSWORD="$WEB_PASS"
+    export PASSWORD="$WEB_PASS"
+fi
+
 exec antiweb --port 8080 --host 0.0.0.0
