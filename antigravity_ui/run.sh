443585ee2516b60f318c36cd40466bcdd8ab19d8
Nick Pardas <pardasnikos@gmail.com>
2026-09-15 13:14:26 +0300

    Define HOME directory for persistent agent storage

--- antigravity_ui/run.sh
+++ antigravity_ui/run.sh
@@ -8,6 +8,10 @@ export PORT=8080
 export HOST=0.0.0.0
 export BIND=0.0.0.0
 
+# Define HOME for Antigravity (persistent storage)
+export HOME="/config/antigravity_home"
+mkdir -p "$HOME"
+
 if [ -n "$WEB_PASS" ] && [ "$WEB_PASS" != "null" ]; then
     bashio::log.info "Web password is set by user."
     export ANTIWEB_PASSWORD="$WEB_PASS"
